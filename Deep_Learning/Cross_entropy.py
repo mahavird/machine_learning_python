160b6aebfc839234844f0b64a67169f08675c45c
mahavird <mahaviredx@gmail.com>
2017-08-14 16:00:29 +0530

    Neural_Network

--- Deep_Learning/Cross_entropy.py
+++ Deep_Learning/Cross_entropy.py
@@ -1,6 +1,10 @@
 import numpy as np
-
+import math
 # Write a function that takes as input two lists Y, P,
 # and returns the float corresponding to their cross-entropy.
 def cross_entropy(Y, P):
-    pass
+    cross_entropy_t=0
+    for i in range(len(Y)):
+        cross_entropy_t = cross_entropy_t - float (Y[i]*math.log(P[i])+(1-Y[i])*(math.log(1-P[i])))
+    print cross_entropy_t
+    return cross_entropy_t
